721fd5f4b008fe7e9933718b2f064b276af8cc72
aliciadoung <aliciadoung8@gmail.com>
2024-04-21 22:35:50 -0400

    home page updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import Navbar from "../components/Navbar"
 
 const Home = () => {
+  const learnMore = () => {
+    console.log('Learn More button clicked');
+    // Add any additional functionality here
+  };
   return (
     <div>
       <Navbar />
-      Home</div>
+      <section id="mainpage">
+        <h1 id="mainpage_title">
+          Share your knowledge with the Boston student community
+        </h1>
+        <p id="mainpage_blurb">
+          Start your financial journey towards personal budgeting success
+        </p>
+        <button onClick={learnMore}>Learn More</button>
+      </section></div>
   )
 }
 
