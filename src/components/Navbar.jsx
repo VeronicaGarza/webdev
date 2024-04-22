fef3bcf3476e4606b94aaa7b4acb2e848e94d287
Veronica Garza <veronicagarza@veronicas-mbp-8.bc.edu>
2024-04-21 22:37:15 -0400

    about and nav fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return (
     <nav>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/services">Services</Link></li> 
-        </ul>
+      <ul className="nav-options">
+        <li><a href="/">Home</a></li>
+        <li><a href="/services">Services</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
     </nav>
   )
 }
