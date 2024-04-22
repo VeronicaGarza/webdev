7b2a6ede60495fa8e53fd2d461a0686c69023c4d
Veronica Garza <veronicagarza@veronicas-mbp-8.bc.edu>
2024-04-22 09:52:49 -0400

    nav bar link fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ const Navbar = () => {
   return (
     <nav>
       <ul className="nav-options">
-        <li><a href="/">Home</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   )
