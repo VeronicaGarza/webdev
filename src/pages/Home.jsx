f21aeb54e9ccd19be75c680f4e14ab8415232b5c
Veronica Garza <veronicagarza@veronicas-mbp-8.bc.edu>
2024-04-28 21:22:09 -0400

    homepage photo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,8 @@ const Home = () => {
         <p id="mainpage_blurb">
           Feel empowered to take charge of your financial future. Planning for tomorrow while enjoying the present is key. Your success story starts here.
         </p>
-      </section>
-      <section className="photo">
-            <img
-              src="https://cdn.glitch.global/61ed9e23-9b4a-4a20-83b1-e45aff56fc8f/people.png?v=1714350794955"
+        <img
+              src="https://cdn.glitch.global/61ed9e23-9b4a-4a20-83b1-e45aff56fc8f/people.svg?v=1714353619362"
               alt="boxes"
               className="boxes"
             />
